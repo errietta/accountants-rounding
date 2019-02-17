d171044503e867b8447d9bed7562ebfc4d6a3839
Errietta Kostala <errietta@errietta.me>
2019-02-17 19:04:25 +0000

    traaavis?

---
+++ deploy_script.sh
@@ -0,0 +1,18 @@
+if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+
+  cd service
+  npm install
+  sls deploy
+  cd ../frontend
+  npm install
+  npm run build
+
+
+  aws s3 sync dist/ s3://accountants-rounding.com
+  aws configure set preview.cloudfront true
+  # Invalidate every object in the targeted distribution.
+  aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
+
+else
+  echo "Skipping deployment, as this is not a tagged commit"
+fi
